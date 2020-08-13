86528d6787265fd33df6e6b43fccb3f18b3718a9
yaelElinav <yaelelinav@gmail.com>
2020-08-13 23:04:38 +0300

    Initial commit with basic code

---
+++ Main.py
@@ -0,0 +1,12 @@
+# TODO: copy the code from files to here for submission
+# For now it will remain like this for comfort
+import BigCross1
+import BigCross2
+
+
+def main():
+    print("I just love matrix")
+
+
+if __name__ == "__main__":
+    main()
